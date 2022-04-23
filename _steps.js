6b6e4935f6391a2a39db1cf602be95de07dde260
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-23 16:43:47 -0700

    created genius car server

---
+++ _steps.js
@@ -0,0 +1,28 @@
+/* 
+1.mkdir geinus-car0service-server
+2.npm init -y
+3.index.js
+4.package.json edit
+5.npm i express cors mongodb dotenv
+6.require all(got to npm dotenv)
+7.use middleware
+-------------------------
+DATABASE CONNECTION
+---------------------------
+1.search mongodb atlas
+2.google login
+3.created database and collection
+3.inserted service .json to database
+4.search node mongodb crud fundamentals
+5.database->connect-->connect your app
+6.to get passwprd database access
+7..env te password rakhci 
+----------------------------
+CRUD OPERATION
+--------------------
+1.data load korar jonno find multiple
+2.
+
+
+
+ */
